5856d410ef6da6776192fbe1a1de6149bb08b2c5
Sam Hamm <sam@codefellows.com>
2018-10-16 10:23:47 -0700

    After first hour

--- class-07-oop-constructors-tables/buildout-of-yesterday/js/app.js
+++ class-07-oop-constructors-tables/buildout-of-yesterday/js/app.js
@@ -1,3 +1,58 @@
 'use strict';
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// 1st and Pike	23	65	6.3
+
+var pike = {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiesPerCustomer: 6.3,
+  locationName: 'First and Pike',
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalDailyCookies: 0
+};
+
+pike.calcCustomersEachHour = function() {
+  for(var i = 0; i < hours.length; i++) {
+    // calculate a random number
+    var randomCustomers = random(this.minCustomersPerHour, this.maxCustomersPerHour);
+    // put that random number into the appropriate array
+    this.customersEachHour.push(randomCustomers);
+  }
+}
+
+pike.calcCookiesEachHour = function() {
+  pike.calcCustomersEachHour();
+  for(var i = 0; i < hours.length; i++) {
+    // calc the cookies
+    var hourlyCookies = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerCustomer);
+    // put the numbers into an array
+    this.cookiesEachHour.push(hourlyCookies);
+    this.totalDailyCookies += hourlyCookies;
+  }
+}
+
+pike.render = function() {
+  this.calcCookiesEachHour();
+  var pikeUl = document.getElementById('pike');
+  // console.log(pikeUl, '<pikeUl>');
+  for(var i = 0; i < hours.length; i++){
+    // create element
+    var liEl = document.createElement('li');
+    // give it content of "6am: 16 cookies"
+    liEl.textContent = `${hours[i]}: ${this.cookiesEachHour[i]} cookies`;
+    // append it
+    pikeUl.appendChild(liEl);
+  }
+  liEl = document.createElement('li');
+  liEl.textContent = `Total: ${this.totalDailyCookies} cookies`;
+  pikeUl.appendChild(liEl);
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; // from MDN docs
+}
+
+pike.render();
---
+++ class-07-oop-constructors-tables/constructor-function-demo/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Let's start by making an object literal of a student, with the following properties: course, enrolled (T/F), instructor, TAs, first name, last name, preferred name, hometown, introduction.
+
+// Constructor function syntax
+
+// function ConstructorFunctionName (parameter1, parameter2) {
+//   this.property = parameter1;
+//   this.property2 = parameter2;
+//   this.staticProperty1 = 'ExampleString';
+//   this.method1 = function () {
+//     console.log('do stuff');
+//   };
+// }
+
+//===================================================
+// Student Constructor
+//===================================================
+
+//==================
+//Student Prototypes
+//==================
+
+// new Student instances
