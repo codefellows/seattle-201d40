00753a49a681edce5925d5d91d8f3bcadf68ce9a
Sam Hamm <sam@codefellows.com>
2018-10-22 11:34:49 -0700

    Publishes lab

--- class-11-av-practical-clicktracker/demo-goat-clicker/app.js
+++ class-11-av-practical-clicktracker/demo-goat-clicker/app.js
@@ -5,7 +5,10 @@ var allGoats = [];
 
 // Make a constructor function
 function Goat(name, filepath) {
-
+  this.name = name;
+  this.filepath = filepath;
+  this.views = 0;
+  allGoats.push(this);
 }
 
 // make new Goat instances
@@ -21,17 +24,24 @@ new Goat('Tongue goat', 'img/tongue-goat.jpg');
 new Goat('Pushy goat', 'img/pushy-goat.jpg');
 
 // Get the <img> element from the DOM
+var goatImg = document.getElementById('goat-pic');
 
 // Write a function to randomly display one of the pictures
 function randomGoat() {
-  // We need to access the <img> element in the DOM
   // Select a random goat from the array of goats
-  // console.log(Goat.allGoats[randomGoat]);
+  var idx = Math.floor(Math.random() * allGoats.length);
+  // console.log(allGoats[idx]);
   // Assign the src, alt, and title attributes to the <img> element
+  goatImg.src = allGoats[idx].filepath;
+  goatImg.alt = allGoats[idx].name;
+  goatImg.title = allGoats[idx].name;
   // Console log which goat is showing
+  console.log(`${allGoats[idx].name} is showing`);
+  // Tally views for each goat
+  allGoats[idx].views++;
 }
 
-
+randomGoat();
 
 // Listen for clicks on the goat and then display a new goat
-
+goatImg.addEventListener('click', randomGoat);
